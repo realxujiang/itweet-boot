e5ae57ad1093a9b810b7b605ee373c99e6e9212d
sundafei <sundafei@redhadoop.com>
2017-03-01 14:52:16 +0800

    add file sundafei

---
+++ src/main/java/cn/itweet/modules/backend/service/LoginService.java
@@ -0,0 +1,11 @@
+package cn.itweet.modules.backend.service;
+
+/**
+ * 说明：
+ * 包名：cn.itweet.modules.backend.service
+ * 项目名：itweet-boot
+ * 创建人：孙大飞
+ * 创建时间：2017/3/1.
+ */
+public class LoginService {
+}
---
+++ src/main/java/cn/itweet/modules/backend/web/LoginController.java
@@ -0,0 +1,11 @@
+package cn.itweet.modules.backend.web;
+
+/**
+ * 说明：
+ * 包名：cn.itweet.modules.backend.web
+ * 项目名：itweet-boot
+ * 创建人：孙大飞
+ * 创建时间：2017/3/1.
+ */
+public class LoginController {
+}
