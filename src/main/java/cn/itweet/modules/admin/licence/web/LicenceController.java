070197c5aba3f5a9077f3b6c3d59b6b67e6bb2e5
SC-201703152226\Administrator <sundafei@redoop.com>
2017-03-14 23:19:19 +0800

    edit licence sundafei

--- src/main/java/cn/itweet/modules/admin/licence/web/LicenceController.java
+++ src/main/java/cn/itweet/modules/admin/licence/web/LicenceController.java
@@ -1,9 +1,13 @@
 package cn.itweet.modules.admin.licence.web;
 
+import cn.itweet.modules.admin.licence.entity.Licence;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 说明：Licence Controller
@@ -13,22 +17,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * 创建时间：2017/3/14.
  */
 @Controller
-@RequestMapping("/admin/licence/")
+@RequestMapping(value = "/admin/licence")
 public class LicenceController {
 
 
-    @RequestMapping(value="list", method = RequestMethod.GET)
+    @RequestMapping(value="/list", method= RequestMethod.GET)
     public String list(){
         return "admin/licence/list";
     }
 
-    @RequestMapping(value="add", method = RequestMethod.GET)
-    public String addPre(){
+    @RequestMapping(value="/add", method= RequestMethod.GET)
+    public String add(Model model, HttpServletRequest request){
         return "admin/licence/add";
     }
 
-    @RequestMapping(value="add", method = RequestMethod.POST)
-    public String add(){
+    @RequestMapping(value="/add", method = RequestMethod.POST)
+    public String add(Model model,Licence licence, HttpServletRequest request){
+        System.out.println("SSSSSSSSSSSSSSSSSSSSSSS");
+
+
         return "redirect:/admin/licence/list";
     }
 }
