e3a31e85ef02b728f5541e82127b8baa58247e95
sundafei <sundafei@redhadoop.com>
2017-04-27 13:43:57 +0800

    edit js sundafei

--- src/main/resources/static/style/front/theme/effe/js/scrolltopcontrol.js
+++ src/main/resources/static/style/front/theme/effe/js/scrolltopcontrol.js
@@ -4,12 +4,16 @@
 //** 1) Adds ability to scroll to an absolute position (from top of page) or specific element on the page instead.
 //** 2) Fixes scroll animation not working in Opera. 
 
-
 var scrolltotop={
+
+
+
+
+
 	//startline: Integer. Number of pixels from top of doc scrollbar is scrolled before showing control
 	//scrollto: Keyword (Integer, or "Scroll_to_Element_ID"). How far to scroll document up when control is clicked on (0=top).
 	setting: {startline:100, scrollto: 0, scrollduration:1000, fadeduration:[500, 100]},
-	controlHTML: '<img src="../images/up.png" style="width:48px; height:48px" />', //HTML for control, which is auto wrapped in DIV w/ ID="topcontrol"
+	controlHTML: '<img src="style/front/theme/effe/images/up.png" style="width:48px; height:48px" />', //HTML for control, which is auto wrapped in DIV w/ ID="topcontrol"
 	controlattrs: {offsetx:5, offsety:5}, //offset of control relative to right/ bottom of window corner
 	anchorkeyword: '#top', //Enter href value of HTML anchors on the page that should also act as "Scroll Up" links
 
